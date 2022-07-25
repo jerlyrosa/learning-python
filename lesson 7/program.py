7311daa2e4a74a6f6cade64ee5e17a7f15f385f3
sr-jerly <jerlymigueldelarosah@gmail.com>
2022-07-25 11:45:50 -0400

    lesson 6:use of lists

---
+++ lesson 7/program.py
@@ -0,0 +1,46 @@
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+
+print("The first planet is", planets[0])
+print("The second planet is", planets[1])
+print("The third planet is", planets[2])
+
+
+number_of_planets = len(planets)
+print("There are", number_of_planets, "planets in the solar system.")
+
+# Output:
+# There are 8 planets in the solar system
+
+
+planets.append("Pluto")
+number_of_planets = len(planets)
+print("There are actually", number_of_planets, "planets in the solar system.")
+
+# Output:
+# There are actually 9 planets in the solar system.
+
+
+jupiter_index = planets.index("Jupiter")
+print("Jupiter is the", jupiter_index + 1, "planet from the sun")
+
+# Output
+# Jupiter is the 5 planet from the sun
+
+
+
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+planets_before_earth = planets[0:2]
+print(planets_before_earth)
+
+# Output: ['Mercury', 'Venus']
+
+
+
+amalthea_group = ["Metis", "Adrastea", "Amalthea", "Thebe"]
+galilean_moons = ["Io", "Europa", "Ganymede", "Callisto"]
+
+regular_satellite_moons = amalthea_group + galilean_moons
+print("The regular satellite moons of Jupiter are", regular_satellite_moons)
+
+# Output
+# The regular satellite moons of Jupiter are ['Metis', 'Adrastea', 'Amalthea', 'Thebe', 'Io', 'Europa', 'Ganymede', 'Callisto']
